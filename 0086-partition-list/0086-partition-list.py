55d57243dc00878740c0c2017ee887e26c84b955
Swayam Maitra <swayumm02@gmail.com>
2026-06-28 01:44:54 +0530

    Time: 0 ms (100%), Space: 19.3 MB (36.58%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.py
@@ -0,0 +1,26 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        before_head = ListNode(0)
+        before = before_head
+        after_head = ListNode(0)
+        after = after_head
+        
+        current = head
+        while current:
+            if current.val < x:
+                before.next = current
+                before = before.next
+            else:
+                after.next = current
+                after = after.next
+            current = current.next
+            
+        after.next = None
+        before.next = after_head.next
+        
+        return before_head.next
